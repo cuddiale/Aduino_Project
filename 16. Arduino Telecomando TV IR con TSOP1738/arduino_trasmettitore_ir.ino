0de212ff14a4766bdf4c271c35508bd315d54435
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-07-07 10:07:49 +0200

    update

---
+++ 16. Arduino Telecomando TV IR con TSOP1738/arduino_trasmettitore_ir.ino
@@ -0,0 +1,26 @@
+#include <IRremote.h> // use the library
+int receiver = 11; // pin 1 of IR receiver to Arduino digital pin 11
+IRrecv irrecv(receiver); // create instance of 'irrecv'
+decode_results results;
+
+void setup()
+{
+  Serial.begin(9600); // for serial monitor output
+  irrecv.enableIRIn(); // Start the receiver
+  pinMode(9, OUTPUT); // Pin 9 output
+}
+void loop()
+{
+  if (irrecv.decode(&results)) // have we received an IR signal?
+  {
+    Serial.println(results.value, HEX); // display it on serial monitor in hexadecimal
+    irrecv.resume();// receive the next value
+  }
+ 
+  if ( results.value == 0x406 ||  results.value == 0x10406 ){ //tasto 6 sul telecomando
+    digitalWrite(9, HIGH);   // set the LED on
+  } 
+ if ( results.value == 0x404 ||  results.value == 0x10404 ){ //tasto 4 sul telecomando
+    digitalWrite(9, LOW);   // set the LED 0ff
+  }  
+}
